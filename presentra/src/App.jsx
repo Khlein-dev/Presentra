8024b858320008d9e3a2118420a3594928276d35
khlein_dev <kierjerymiepascua@gmail.com>
2026-03-01 11:15:29 +0800

    latest update

--- presentra/src/App.jsx
+++ presentra/src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Session from "./pages/Session";
 import Dashboard from "./pages/Dashboard";
+import Footer from "./components/Footer";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/session" element={<Session />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/session" element={<Session />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        <Footer />
     </Router>
   );
 }
