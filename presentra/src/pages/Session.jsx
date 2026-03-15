2344962fc2120fe092f83588a09fd5692e30aab2
zenji <cabrera.arthurbenedict13@gmail.com>
2026-03-15 23:45:06 +0800

    memorize toggle

--- presentra/src/pages/Session.jsx
+++ presentra/src/pages/Session.jsx
@@ -1022,10 +1022,9 @@ function Session() {
     const toggleMemorizeMode = () => {
         setIsMemorizeMode((prev) => {
             const next = !prev;
-
+            // NEW: Direct toggle visibility on button press
+            setIsScriptVisible(next ? false : true);  // Hide if memorize ON, show if OFF
             clearTimeout(hideScriptTimeoutRef.current);
-            setIsScriptVisible(true);
-
             return next;
         });
     };
